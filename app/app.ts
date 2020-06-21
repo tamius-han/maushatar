6cc9529b08aa49f97db74ee5a6fa554100b8e8c3
Tamius Han <ziga.pirih@hotmail.com>
2020-06-21 18:06:10 +0200

    Add boilerplate code

---
+++ app/app.ts
@@ -0,0 +1,37 @@
+import * as Discord from 'discord.js';
+import env from './env/env';
+import { ensureDirSync } from './lib/fs-helpers';
+import CommandProcessor from './command-processor/command-processor';
+
+// prepare data directories
+ensureDirSync(env.voiceRecordingDir);
+
+// initiate discord client & command processor
+const client = new Discord.Client();
+const commandProcessor = new CommandProcessor();
+
+client.once('ready', () => {
+  console.log('Client ready.');
+})
+client.once('error', (e) => {
+  console.log('There\'s been an error:', e);
+}) 
+client.once('disconnect', () => {
+  console.log('Client disconnected.');
+})
+
+// process message
+client.on('message', async message => {
+  if (message.author.bot) {
+    return;
+  }
+
+  for (const p of env.prefixes) {
+    if (message.content.startsWith(p)) {
+      commandProcessor.processCommand(message, message.content.substring(p.length).trim());
+    };
+  }  
+
+});
+
+client.login(env.token);
---
+++ app/command-processor/command-processor.ts
@@ -0,0 +1,8 @@
+import * as Discord from 'discord.js';
+
+export default class CommandProcessor {
+  
+  async processCommand(message: Discord.Message, commandString: string) {
+
+  }
+}
