97472cb1d65a3d54f04096121d29426edaa5307b
Tamius Han <ziga.pirih@hotmail.com>
2020-06-23 21:26:28 +0200

    Example env file

---
+++ app/env/example.env.ts
@@ -0,0 +1,7 @@
+export default {
+  prefixes: [":logger", ":l"],
+  token: "INSERT_DISCORD_TOKEN_HERE",
+
+  voiceRecordingDir: '/tmp/.maushatar/voice-data',
+  deepspeechModelDir: '/PATH/TO/TRAINED/model.pbmm' 
+}
