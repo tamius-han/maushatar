445f3442eff37c0a4d51e666243d2d93c728f2f1
Tamius Han <ziga.pirih@hotmail.com>
2020-06-30 02:50:01 +0200

    add new variables

--- app/env/example.env.ts
+++ app/env/example.env.ts
@@ -18,18 +18,27 @@ export default {
   // voice recordings cannot be made.
   playOnJoin: '/path/to/file',
 
-
   // Speech-to-text settings
   //
   // enableSTT options:
   //  * false: no STT
   //  * 'deepspeech': use Mozilla DeepSpeech
+  //  * 'julius':     use julius
   enableSTT: false,
+
+  // post trasncription to this channel
+  // leave empty to post in channel the bot was given command in
+  // set to false (boolean) to disable stt sending
+  sttChannel: 'channel-name-without-hash',
 
   // Directory in which voice recordings will be kept.
   // can be same folder as voiceRecordingsDir, but only if recordingFormat
   // is opus.
   STTRecordingDir: '/PATH/TO/FOLDER',
+  
+  // Directory in which temporary files for STT processing will be kept
+  // (if there will be any)
+  STTTmpDir: '/PATH/TO/FOLDER',
   
   // if set to 'false', recordings will be deleted as soon as STT is complete.
   // this value controls deletion of 32bit PCMs discord makes
@@ -43,4 +52,10 @@ export default {
   //
   // if enableDeepSpeech is disabled, this can be anything as long as it's not missing
   deepspeechModelDir: '/PATH/TO/TRAINED/model.pbmm',
+
+  // julius-related settings
+  juliusBinaryDir: '',    // julius executable
+  juliusDnnDir: '',       // folder with julius models. Julius is started in this directory.
+  juliusJconfDir: '',     // julius jconf file
+  juliusDnnJconfDir: '',  // julius dnn jconf file
 }
